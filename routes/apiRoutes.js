9622cbab91b96e4e5235da2c9f09c42d3cb40d7b
Joseph Pappano <jepappano4@Josephs-MacBook-Pro.local>
2019-02-06 22:02:58 -0600

    finished api routes for both tables

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,29 +2,99 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
+
+  // Artists API calls
   app.get("/api/artists", function(req, res) {
     db.Artist.findAll({}).then(function(results) {
       res.json(results);
     });
   });
 
-//   app.get("/merch", function(req, res) {
-//     db.Example.findAll({}).then(function(merch) {
-//       res.json(merch);
-//     });
-//   });
+  app.post("/api/newArtist", function(req, res) {
+    db.Artist.create({
+      Artist: "Black Sabbath",
+      Genre: "Metal",
+      Website: "www.blacksabbath.com",
+      Logo: "url.jpeg"
+    }).then(function(results) {
+      res.json(results)
+    });
+  });
 
-  // Create a new example
-//   app.post("/api/examples", function(req, res) {
-//     db.Example.create(req.body).then(function(dbExample) {
-//       res.json(dbExample);
-//     });
-//   });
 
-  // Delete an example by id
-//   app.delete("/api/examples/:id", function(req, res) {
-//     db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-//       res.json(dbExample);
-//     });
-//   });
+  app.post("/api/updateArtist", function(req, res) {
+    db.Show.update(
+      {
+        Logo: "myPic.jpeg"
+      },
+      {
+        where: {
+          id: 2
+        }
+      }
+    ).then(function(results) {
+      res.json(results);
+    })
+  });
+
+  app.post("/api/deleteArtist", function(req, res) {
+    db.Artist.destroy({
+      where: {
+        id: 1
+      }
+    }).then(function(results) {
+      res.redirect("/api/events")
+    });
+  });
+
+
+// Events API calls
+app.get("/api/events", function(req, res) {
+  db.Show.findAll({}).then(function(results) {
+    res.json(results)
+  });
+});
+
+app.post("/api/newEvent", function(req, res) {
+  db.Show.create({
+    Venue: "Star Bar",
+    Date: "12/27/2019",
+    Price: "15",
+    Facebook: "www.facebook.com/event",
+    // ArtistId: "artistsId"
+  }).then(function(results) {
+    res.json(results)
+  });
+});
+
+app.post("/api/updateEvent", function(req, res) {
+  db.Show.update(
+    {
+      Venue: "stubbs"
+    },
+    {
+      where: {
+        id: 2
+      }
+    }
+  ).then(function(results) {
+    res.json(results);
+  })
+});
+
+app.post("/api/deleteEvent", function(req, res) {
+  db.Show.destroy({
+    where: {
+      id: 3
+    }
+  }).then(function(results) {
+    res.redirect("/api/events")
+  });
+});
+
+
+
+
 };
+
+
