720f838e340dcb7a27f252362f98710172dc1d98
Joseph Pappano <jepappano4@Josephs-MacBook-Pro.local>
2019-02-09 14:05:00 -0600

    making an events page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -22,6 +22,10 @@ module.exports = function(app) {
     res.render("events");
   });
 
+  app.get("/createEvents", function(req, res) {
+    res.render("createEvent");
+  });
+
   // // blog route loads blog.html
   // app.get("/blog", function(req, res) {
   //   res.sendFile(path.join(__dirname, "../public/blog.html"));
